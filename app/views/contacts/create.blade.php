edaedea1f6c9a3db760bbba2b7bd0d09b2126ed2
tavaresoft <joe@tavaresoft.com>
2015-01-25 18:06:58 -0500

    Create Contact
    Cosmetics

--- app/views/contacts/create.blade.php
+++ app/views/contacts/create.blade.php
@@ -1,8 +1,22 @@
+@extends('layouts.master')
+
+@section('right_menu')
+
+{{View::make('layouts.admin_menu')}}
+
+@stop
+
+
+
+@section('content')
+
+<br><br><br>
+
 {{ Form::open(array('action' => 'ContactsController@store', 'files' => true)) }}
 
 
 <div class="row">
-		<div class="small-6 columns"><div style="color:#000000">
+		<div class="large-6 columns"><div style="color:#000000">
 			<select name="type">
 				<option>Choose Contact Type..GENERATE FROM LISTTTTTT..</option>
 				<option value="ARTIST">Artist</option>
@@ -11,7 +25,7 @@
 
 			</div>
 		</div>
-	<div class="small-6 columns">{{Form::text('nickname', '', array('placeholder' => 'Alias'))}}</div>
+	<div class="large-6 columns">{{Form::text('nickname', '', array('placeholder' => 'Alias'))}}</div>
 </div>
 
 
@@ -19,43 +33,46 @@
 
 
 <div class="row">
-	<div class="small-6 columns">{{Form::text('fname', '', array('placeholder' => 'First Name'))}}</div>
-	<div class="small-6 columns">{{Form::text('lname', '', array('placeholder' => 'Last Name'))}}</div>
-</div>
-
-<div class="row">
-	<div class="small-6 columns">{{Form::text('phone', '', array('placeholder' => 'Phone'))}}</div>
-	<div class="small-6 columns">{{Form::text('email', '', array('placeholder' => 'E-mail'))}}</div>
+	<div class="large-6 columns">{{Form::text('fname', '', array('placeholder' => 'First Name'))}}</div>
+	<div class="large-6 columns">{{Form::text('lname', '', array('placeholder' => 'Last Name'))}}</div>
 </div>
 
 
 <div class="row">
-	<div class="small-6 columns">{{Form::text('street1', '', array('placeholder' => 'Street Address'))}}</div>
-	<div class="small-6 columns">{{Form::text('street2', '', array('placeholder' => 'Street Address (apt, ste, etc.)'))}}</div>
+	<div class="large-6 columns">{{Form::text('street1', '', array('placeholder' => 'Street Address'))}}</div>
+	<div class="large-6 columns">{{Form::text('street2', '', array('placeholder' => 'Street Address (apt, ste, etc.)'))}}</div>
 </div>
 
 
 <div class="row">		
-	<div class="small-6 columns">{{Form::text('city', '', array('placeholder' => 'City'))}}</div>
+	<div class="large-6 columns">{{Form::text('city', '', array('placeholder' => 'City'))}}</div>
 	
-	<div class="small-3 columns">{{Form::text('state', '', array('placeholder' => 'State'))}}</div>
-	<div class="small-3 columns">{{Form::text('zip', '', array('placeholder' => 'Zip Code'))}}</div>
+	<div class="large-3 columns">{{Form::text('state', '', array('placeholder' => 'State'))}}</div>
+	<div class="large-3 columns">{{Form::text('zip', '', array('placeholder' => 'Zip Code'))}}</div>
+</div>
+
+
+<div class="row">
+	<div class="large-1 columns"><span class="icon-phone"></span></div>
+		<div class="large-5 columns">{{Form::text('phone')}}</div>
+	<div class="large-1 columns"><span class="icon-envelope"></span></div>
+		<div class="large-5 columns">{{Form::text('email')}}</div>
 </div>
 
 <div class="row">
 	
-	<div class="small-1 columns"><span class="icon-facebook"></span></div>
-		<div class="small-5 columns">{{Form::text('facebook', '', array('placeholder' => 'Paste Facebook link here'))}}</div>
+	<div class="large-1 columns"><span class="icon-facebook"></span></div>
+		<div class="large-5 columns">{{Form::text('facebook', '', array('placeholder' => 'Paste Facebook link here'))}}</div>
 	
-	<div class="small-1 columns"><span class="icon-twitter"></span></div>
-		<div class="small-5 columns">{{Form::text('twitter', '', array('placeholder' => 'Paste Twitter link here'))}}</div>
+	<div class="large-1 columns"><span class="icon-twitter"></span></div>
+		<div class="large-5 columns">{{Form::text('twitter', '', array('placeholder' => 'Paste Twitter link here'))}}</div>
 </div>
 
 <div class="row">	
-	<div class="small-1 columns"><span class="icon-instagram"></span></div>	
-		<div class="small-5 columns">{{Form::text('instagram', '', array('placeholder' => 'Paste IG link here'))}}</div>
-	<div class="small-1 columns"><span class="icon-globe"></span></div>		
-		<div class="small-5 columns">{{Form::text('website', '', array('placeholder' => 'Pase Website link here'))}}</div>
+	<div class="large-1 columns"><span class="icon-instagram"></span></div>	
+		<div class="large-5 columns">{{Form::text('instagram', '', array('placeholder' => 'Paste IG link here'))}}</div>
+	<div class="large-1 columns"><span class="icon-globe"></span></div>		
+		<div class="large-5 columns">{{Form::text('website', '', array('placeholder' => 'Pase Website link here'))}}</div>
 
 
 </div>
@@ -70,4 +87,7 @@
 
 {{Form::close()}}
 
+<br><br>
 
+
+@stop
